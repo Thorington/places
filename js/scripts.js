88873111664c9a0b8f178d326b9fae6fe9ff2dc5
Claire Thorington <claire.thorington@gmail.com>
2022-06-13 15:10:08 -0700

    WIP commit
    Co-authored-by: Alex Shevlin <alexshevlin1@gmail.com>
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,66 @@
 // Business Logic
+class Places {
+  constructor(location, landmark, season, advice) {
+    this.location = location;
+    this.landmark = landmark;
+    this.season = season;
+    this.advice = advice;
+  }
 
-function Place(location, landmark, season, advice) {
-  this.location = location;
-  this.landmark = landmark;
-  this.season = season;
-  this.advice = advice;
+    // let placeArray = [this.location,this.landmark,this.season,this.advice]
+  display() {
+    let placeString = this.location + " " + this.landmark + " " + this.season + " " + this.advice;
+    return placeString;
+  }
 }
 
-let portland = new Place("Oregon", "Welcome to Portland, Oregon sign", "Fall", "Avoid SE Powell");
-let edinburgh = new Place("Scotland", "Edinburgh Castle", "Spring", "Avoid the haggis");
-let munich = new Place("Germany", "Olympic Stadium", "Fall", "Avoid Oktoberfest");
-
+let portland = new Places("Oregon", "Welcome to Portland, Oregon sign", "Fall", "Avoid SE Powell");
+let edinburgh = new Places("Scotland", "Edinburgh Castle", "Spring", "Avoid the haggis");
+let munich = new Places("Germany", "Olympic Stadium", "Fall", "Avoid Oktoberfest");
 
 // UI Logic
+
+$(document).ready(function() {
+
+  const listPlace = document.getElementById("place");
+  
+  document.getElementById("place").addEventListener('click', function() {
+    document.getElementById("location").innerHTML = portland.display(); 
+  });
+})
+
+// '<ul><li>' + placeArray.join("</li><li>"); + '</li></ul>';
+
+// function displayContactDetails(addressBookToDisplay) {
+//   let contactsList = $("ul#contacts");
+//   let htmlForContactInfo = "";
+//   Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+//     const contact = addressBookToDisplay.findContact(key);
+//     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+//   });
+//   contactsList.html(htmlForContactInfo);
+// }
+
+// $(document).ready(function() {
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//     const inputtedFirstName = $("input#new-first-name").val();
+//     const inputtedLastName = $("input#new-last-name").val();
+//     const inputtedPhoneNumber = $("input#new-phone-number").val();
+//     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+//     addressBook.addContact(newContact);
+//     displayContactDetails(addressBook)
+//   });
+// });
+
+
+
+
+// $(document).ready(function(){
+//   $("form#counter").submit(function(event){
+//     event.preventDefault();
+//     const number = $("#number").val ();
+//     const result = output(number);
+//     $("#output").toggle(Place);
+//   });
+// });
