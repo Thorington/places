11c04fb350807fff81c03ed20b5361337cf03b28
Claire Thorington <claire.thorington@gmail.com>
2022-06-13 10:47:09 -0700

    create initial business logic
    Co-authored-by: Alex Shevlin <alexshevlin1@gmail.com>
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic
+
+function Place(location, landmark, season, advice) {
+  this.location = location;
+  this.landmark = landmark;
+  this.season = season;
+  this.advice = advice;
+}
+
+let portland = new Place("Portland", "Welcome to Portland, Oregon sign", "Fall", "Avoid SE Powell");
+
+
+
+
+// UI Logic
